42e55f24bf2d7f12bcdca2ce9bacd2500677054e
Jimmy DC <jimmydalecleveland@gmail.com>
2020-07-02 13:05:13 -0600

    add react specific setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,23 @@ module.exports = {
   // env propertiers are added by the eslint bootstrap process but
   // not necessarily needed.
   env: {
-    browser: true,
     es2020: true,
   },
   // Only use prettier in `extends` not in `plugins:
   // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 11,
     sourceType: 'module',
   },
+  plugins: ['react'],
   rules: {
     // I prefer my formatting rules to be warnings, rather than errors,
     // so I don't think I have errors in my code while writing it.
